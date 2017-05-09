f6d1583c8a4bad23ab67d02e62f918b353e6bc45
liyongyao <731969968@qq.com>
2017-05-09 23:04:46 +0800

    01day

---
+++ sample/c1.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+"""
+python version:3.5
+这是学习python的第一课, python 数据类型
+"""
+
+__author__ = "Liyy"
+
+
+
+# 输出字符串
+print("hello, world!")
+# 输出 数学计算
+print(100 + 200 + 300)
+# 字符串拼接输出
+print("My name","is zhangsan,I am","18 years old!")
+#输入
+i = input("plese input somethong...")
+print(i)
+print("test end")
